fb0c3bc82cfd5f138198af7205ea1e983847afa1
Gabriela15 <gabriela195487@gmail.com>
2021-04-20 15:14:31 +0800

    t5

--- graph_query.py
+++ graph_query.py
@@ -4,3 +4,22 @@
 
 #https://juejin.cn/post/6844903457053212685
 #https://github.com/zhouyuexie/learn-graphql
+
+{
+ echo(message:"world")
+}
+
+query echo {
+    echo(message: "world test")
+}
+
+query{
+  getFirstPost{title}
+}
+
+query{
+  getFirstPost{
+  title,
+  content,
+  category}
+}
